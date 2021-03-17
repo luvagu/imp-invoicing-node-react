994caa894a6beb2360067770d3198849309f60b9
luvagu <luiavag@gmail.com>
2021-03-16 21:22:59 -0500

    created sidebar component with route links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Sidebar from './components/Sidebar'
+
 function App() {
 	return (
-		<div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-			<img
-				class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0"
-				src="/img/erin-lindford.jpg"
-				alt="Woman's Face"
-			/>
-			<div class="text-center space-y-2 sm:text-left">
-				<div class="space-y-0.5">
-					<p class="text-lg text-black font-semibold">
-						Erin Lindford
-					</p>
-					<p class="text-gray-500 font-medium">Product Engineer</p>
-				</div>
-				<button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-					Message
-				</button>
-			</div>
+		<div className="flex h-screen bg-gray-200">
+			<Router>
+				<Sidebar />
+				<Switch>
+					{/* <Route path="/nueva-factura" component={Invoices} />
+					<Route path="/nueva-proforma" component={Quotes} />
+					<Route path="/nueva-proforma" component={Quotes} />
+					<Route path="/ver-facturas" component={Quotes} />
+					<Route path="/ver-proformas" component={Quotes} />
+					<Route path="/ver-products" component={Quotes} />
+					<Route exact path="/" component={Home} /> */}
+				</Switch>
+			</Router>	
 		</div>
+		
 	)
 }
 
