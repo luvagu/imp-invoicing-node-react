73758ebd8c81a490abfbe62cdc18fab2090f14a0
luvagu <luiavag@gmail.com>
2021-03-19 07:48:24 -0500

    fixed transition not working on modal

--- client/src/components/ProductSearchModal.jsx
+++ client/src/components/ProductSearchModal.jsx
@@ -55,7 +55,7 @@ export default function ProductSearchModal({ show, handleClose, handleAddProduct
 	}
 
 	return (
-		<div className={`${show ? '' : 'hidden'} fixed z-40 top-0 right-0 left-0 bottom-0 h-full w-full bg-black bg-opacity-80 transition-opacity`}>
+		<div className={`fixed z-40 top-0 right-0 left-0 bottom-0 bg-black bg-opacity-80 transition-opacity ${show ? 'h-full w-full opacity-100 visible' : 'h-0 w-0 opacity-0 invisible'}`}>
 			<div className="p-4 max-w-xl mx-auto relative absolute left-0 right-0 overflow-hidden mt-24">
 				<div 
                     className="shadow absolute right-0 top-0 w-10 h-10 rounded-full bg-white text-gray-500 hover:text-gray-800 inline-flex items-center justify-center cursor-pointer"
