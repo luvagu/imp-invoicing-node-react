a042521a50356d45d739d3bd204bb5aefba4963e
luvagu <luiavag@gmail.com>
2021-03-23 18:24:02 -0500

    added api update helper

--- client/src/api/helpers.js
+++ client/src/api/helpers.js
@@ -9,3 +9,8 @@ export const createDocApi = async (apiFolder, docData) => {
     const { data } = await axios.post(`http://localhost:5000/create-doc/${apiFolder}`, docData)
     return data
 }
+
+export const updateDocApi = async (apiFolder, docNum, docData) => {
+    const { data } = await axios.put(`http://localhost:5000/update-doc/${apiFolder}/${docNum}`, docData)
+    return data
+}
