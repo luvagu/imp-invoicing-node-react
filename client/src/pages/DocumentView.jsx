269080d209ad0a5116583a5b1cfb206e56bf5534
luvagu <luiavag@gmail.com>
2021-03-26 21:26:42 -0500

    added doc notes

--- client/src/pages/DocumentView.jsx
+++ client/src/pages/DocumentView.jsx
@@ -6,18 +6,33 @@ import useSearchApi from '../hooks/useSearchApi'
 import ProformaInvoice from './ProformaInvoice'
 
 export default function DocumentView() {
-    const { folder, doc } = useParams()
+	const { folder, doc } = useParams()
 
-    const [{ searchResults: docData, isLoading, errorMsg }, setRouteWithQuery] = useSearchApi()
+	const [{ searchResults: docData, isLoading, errorMsg }, setRouteWithQuery] = useSearchApi()
 
-    useEffect(() => {
-        setRouteWithQuery(`/get-doc/${folder}/${doc}`)
-    }, [folder, doc, setRouteWithQuery])
+	useEffect(() => {
+		setRouteWithQuery(`/get-doc/${folder}/${doc}`)
+	}, [folder, doc, setRouteWithQuery])
 
-    return docData ? <ProformaInvoice docType={docData.docType} apiFolder={folder} docDataReceived={docData} /> : (
-        <div className="container mx-auto px-4 md:px-6 py-4 md:py-6">
-            {isLoading && <div className="mt-8 px-4 text-center"><Spinner /></div>}
-            {errorMsg && <div className="mt-8 px-4 text-center text-sm text-red-600 font-semibold uppercase">{errorMsg}</div>}
-        </div>
-    )
+	return docData ? (
+		<ProformaInvoice
+			docType={docData.docType}
+			apiFolder={folder}
+			docDataReceived={docData}
+			allowNotes={folder === 'proformas' ? true : false}
+		/>
+	) : (
+		<div className="container mx-auto px-4 md:px-6 py-4 md:py-6">
+			{isLoading && (
+				<div className="mt-8 px-4 text-center">
+					<Spinner />
+				</div>
+			)}
+			{errorMsg && (
+				<div className="mt-8 px-4 text-center text-sm text-red-600 font-semibold uppercase">
+					{errorMsg}
+				</div>
+			)}
+		</div>
+	)
 }
