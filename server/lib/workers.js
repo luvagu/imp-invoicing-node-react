35578a1a5b784ed9cc903114af0887973b9969f4
luvagu <luiavag@gmail.com>
2021-03-30 18:59:43 -0500

    added delete expired tokens

--- server/lib/workers.js
+++ server/lib/workers.js
@@ -1,3 +1,4 @@
+const helpers = require('./helpers')
 const { queryDB, updateStats, listDocs, readDoc } = require('./helpers')
 
 // Instantiate workers
@@ -58,20 +59,42 @@ workers.statsUpdate = async () => {
             return console.log('new stats >>>', newStats)
         }
     } catch (error) {
-        return console.error('stats update error >>>', error)
+        return console.error('stats update error >>>', error.message)
     }
 }
 
-// Run statsUpdate every hour
+// Delete expired tokens
+workers.deleteExpiredTokens = async () => {
+    try {
+        const tokensList = await helpers.listDocs('tokens')
+        let deleteCount = 0
+
+        for (const fileName of tokensList) {
+            const tokenData = await helpers.readDoc('tokens', fileName)
+            const isValid = await helpers.verifyToken(tokenData.id, tokenData.user)
+            if (!isValid) {
+                deleteCount++
+                await helpers.deleteDoc('tokens', fileName)
+            }
+        }
+
+        return console.log(`${deleteCount} expired tokens deleted`)
+    } catch (error) {
+        console.error('delete expired tokens error >>>', error.message)
+    }
+}
+
+// Run statsUpdate every half hour
 workers.loop = () => {
     setInterval(() => {
         workers.statsUpdate()
-    }, 1000 * 60 * 60)
+    }, 1000 * 60 * 30)
 }
 
 // Init script
 workers.init = () => {
     workers.loop()
+    workers.deleteExpiredTokens()
 }
 
 module.exports = workers.init
