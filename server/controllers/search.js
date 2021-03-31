0b01b11db720c86658f197b9f2883dcc9626a9de
Luis Vallejo <luiavag@gmail.com>
2021-03-31 18:01:24 -0500

    refactored routes

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,3 +1,83 @@
+const { queryDB } = require("../lib/helpers")
 
+const getSingleClientId = async (req, res) => {
+    const clientId = req.params.query
 
-const 
+    try {
+        const clientData = await queryDB('clients_by_id')
+        const client = clientData[clientId] || null
+
+        if (client !== null) {
+            res.status(200).send([client])
+        } else {
+            res.status(404).send({ error: `ID de cliente no encontrada: ${clientId}` })
+        }
+    } catch (error) {
+        console.error('Error on Get path >>> /search-client-id/:id', error.message)
+        res.status(500).send({ error: 'No se pudo leer la base de datos' })
+    }
+}
+
+const getClientsByName = async (req, res) => {
+    const clientName = req.params.query
+
+    try {
+        const clientData = await queryDB('clients')
+        const clients = clientData.filter(({ name }) => name.toLowerCase().includes(clientName.toLocaleLowerCase()))
+
+        if (clients.length) {
+            res.status(200).send(clients)
+        } else {
+            res.status(404).send({ error: `Nombre de cliente no encontrado: ${clientName}` })
+        }
+    } catch (error) {
+        console.error('Error on Get path >>> /search-client-name/:name', error.message)
+        res.status(500).send({ error: 'No se pudo leer la base de datos' })
+    }
+}
+
+const getSingleProductId = async (req, res) => {
+    const prodId = req.params.query
+
+    try {
+        const products = await queryDB('products_by_id')
+        const product = products[prodId] || null
+
+        if (product !== null) {
+            res.status(200).send([product])
+        } else {
+            res.status(404).send({ error: `Codigo exacto de producto no encontrado: ${prodId}` })
+        }
+    } catch (error) {
+        console.error('Error on Get path >>> /search-product-id/:id', error.message)
+        res.status(500).send({ error: 'No se pudo leer la base de datos' })
+    }
+}
+
+const getProductsWithTerms = async (req, res) => {
+    const query = req.params.query
+    const idRegx = new RegExp(`^${query.toLowerCase()}.*`)
+
+    try {
+        const products = await queryDB('products')
+        const parcialCodes = products.filter(({ id }) => idRegx.test(id.toLowerCase()))
+        const parcialNames = products.filter(({ name }) => name.toLowerCase().includes(query.toLowerCase()))
+        const combinedResults = [ ...parcialCodes, ...parcialNames ]
+
+        if (combinedResults.length) {
+            res.status(200).send(combinedResults)
+        } else {
+            res.status(404).send({ error: `Codigo o nombre parcial de producto no encontrado: ${query}` })
+        }
+    } catch (error) {
+        console.error('Error on Get path >>> /search-product-terms/:terms', error.message)
+        res.status(500).send({ error: 'No se pudo leer la base de datos' })
+    }
+}
+
+module.exports = {
+    getSingleClientId,
+    getClientsByName,
+    getSingleProductId,
+    getProductsWithTerms
+}
--- server/index.js
+++ server/index.js
@@ -3,16 +3,23 @@ const express = require('express')
 const cors = require('cors')
 const compression = require('compression')
 const app = express()
-const helpers = require('./lib/helpers')
-const startWorkers = require('./lib/workers')
+
 const { getToken, postToken, putToken, deleteToken } = require('./controllers/tokens')
 const { getDocList, getDocSingle, postCreateDoc, putUpdateDoc, deleteDocSingle, deleteDocAll } = require('./controllers/docs')
-const { getDocSalesStats, getDocSequences } = require('./controllers/utils')
+const { getDocSalesStats, getDocSequences, putUpdateSequence } = require('./controllers/utils')
+const { getSingleClientId, getClientsByName, getSingleProductId, getProductsWithTerms } = require('./controllers/search')
+
+const startWorkers = require('./lib/workers')
 
 // IMPSRV IP 192.168.1.102
 // SELF IP 192.168.1.5
 const HOST_HTTP = process.env.NODE_ENV === 'production' ? '192.168.1.5' : 'localhost'
 const PORT = process.env.PORT || 5000
+
+// Use middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 // Serve Client Static Dir
 //--GIT--> export NODE_ENV=production
@@ -29,10 +36,6 @@ if (process.env.NODE_ENV === 'production') {
         res.send({ welcome: 'Welcome to IMP Invoicing API' })
     })
 }
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
 
 // Auth routes
 app.get('/tokens/:id', getToken)
@@ -54,90 +57,10 @@ app.get('/doc-sequences', getDocSequences)
 app.put('/update-sequence/:prop/:value', putUpdateSequence)
 
 // Search DB related routes
-app.get('/search-client-id/:query', async (req, res) => {
-    const clientId = req.params.query
-
-    try {
-        const clientData = await helpers.queryDB('clients_by_id')
-        const client = clientData[clientId] || null
-
-        if (client !== null) {
-            res.status(200).send([client])
-        } else {
-            res.status(404).send({ error: `ID de cliente no encontrada: ${clientId}` })
-        }
-    } catch (error) {
-        console.error('Error on Get path >>> /search-client-id/:id', error.message)
-        res.status(500).send({ error: 'No se pudo leer la base de datos' })
-    }
-})
-
-app.get('/search-client-name/:query', async (req, res) => {
-    const clientName = req.params.query
-
-    try {
-        const clientData = await helpers.queryDB('clients')
-        const clients = clientData.filter(({ name }) => name.toLowerCase().includes(clientName.toLocaleLowerCase()))
-
-        if (clients.length) {
-            res.status(200).send(clients)
-        } else {
-            res.status(404).send({ error: `Nombre de cliente no encontrado: ${clientName}` })
-        }
-    } catch (error) {
-        console.error('Error on Get path >>> /search-client-name/:name', error.message)
-        res.status(500).send({ error: 'No se pudo leer la base de datos' })
-    }
-})
-
-// app.get('/get-all-products', async (req, res) => {
-//     try {
-//         const allProducts = await helpers.queryDB('products')
-//         res.status(200).send(allProducts)
-//     } catch (error) {
-//         console.error('Error on Get path >>> /get-all-products', error.message)
-//         res.status(500).send({ error: 'No se pudo leer la base de datos' })
-//     }
-// })
-
-app.get('/search-product-id/:query', async (req, res) => {
-    const prodId = req.params.query
-
-    try {
-        const products = await helpers.queryDB('products_by_id')
-        const product = products[prodId] || null
-
-        if (product !== null) {
-            res.status(200).send([product])
-        } else {
-            res.status(404).send({ error: `Codigo exacto de producto no encontrado: ${prodId}` })
-        }
-    } catch (error) {
-        console.error('Error on Get path >>> /search-product-id/:id', error.message)
-        res.status(500).send({ error: 'No se pudo leer la base de datos' })
-    }
-})
-
-app.get('/search-product-includes/:query', async (req, res) => {
-    const query = req.params.query
-    const idRegx = new RegExp(`^${query.toLowerCase()}.*`)
-
-    try {
-        const products = await helpers.queryDB('products')
-        const parcialCodes = products.filter(({ id }) => idRegx.test(id.toLowerCase()))
-        const parcialNames = products.filter(({ name }) => name.toLowerCase().includes(query.toLowerCase()))
-        const combinedResults = [ ...parcialCodes, ...parcialNames ]
-
-        if (combinedResults.length) {
-            res.status(200).send(combinedResults)
-        } else {
-            res.status(404).send({ error: `Codigo o nombre parcial de producto no encontrado: ${query}` })
-        }
-    } catch (error) {
-        console.error('Error on Get path >>> /search-product-terms/:terms', error.message)
-        res.status(500).send({ error: 'No se pudo leer la base de datos' })
-    }
-})
+app.get('/search-client-id/:query', getSingleClientId)
+app.get('/search-client-name/:query', getClientsByName)
+app.get('/search-product-id/:query', getSingleProductId)
+app.get('/search-product-includes/:query', getProductsWithTerms)
 
 app.listen(PORT, () => {
   console.log(`App listening at http://${HOST_HTTP}:${PORT}`)
