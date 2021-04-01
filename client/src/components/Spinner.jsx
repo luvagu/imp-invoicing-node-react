e727561da1ab980dbc6b5cf65719f6bf69dbccae
luvagu <luiavag@gmail.com>
2021-03-31 22:39:14 -0500

    removed test page

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import { SvgSpinner } from '../icons'
 
 export default function Spinner() {
     return (
-        <div className="p-1 text-white rounded-full bg-yellow-500 inline-flex items-center justify-center" title="Descargar PDF">
+        <div className="p-1 text-white rounded-full bg-yellow-500 inline-flex items-center justify-center" title="Cargando...">
             <SvgSpinner className="animate-spin h-5 w-5" />
         </div>
     )
